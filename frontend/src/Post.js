99dedc5a6307126d49af233c919770c14627ca82
majoh19 <maria.hernandezr@est.iudigital.edu.co>
2024-04-30 01:47:04 -0500

    :sparkles: Start with Pages

---
+++ frontend/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return(
+        <div classname="post">
+            <div classname="image">
+                <img src="https://www.onelovelylife.com/wp-content/uploads/2014/03/zucchini-burgers3.jpg"/>
+            </div>
+            <div classname="texts">
+                <h2>Poppers de Pollo y Calabacín</h2>
+                <p classname="info">
+                    <a classname="author">Emily</a>
+                    <time>2014-03-31</time>
+                </p>
+                <p classname="summary">Albóndigas Paleo de Pollo y Calabacín ¡Son muy fáciles y deliciosas! ¡Perfectas para hacer meal-prep y comer sano!</p>
+            </div>
+        </div>
+    )
+}
