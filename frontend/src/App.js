759a839ff18158057a00a5dcd98f8cfe9597137a
majoh19 <maria.hernandezr@est.iudigital.edu.co>
2024-05-22 02:45:00 -0500

    :sparkles:frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,33 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Header from "./Header";
-import Post from "./Post";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import { UserContextProvider } from "./UserContext";
+import CreateRecipe from "./pages/CreateRecipe";
+import RecipePage from "./pages/RecipePage";
+import EditRecipe from "./pages/EditRecipe";
 
 function App() {
+
   return (
-    <Routes>
-      <Route path="/" element={<Layout />} >
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Route>
-    </Routes>
-  );
+
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<IndexPage/>}/>
+          <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/create" element={<CreateRecipe/>}/>
+          <Route path="/recipe/:id" element={<RecipePage/>}/>
+          <Route path="/edit/:id" element={<EditRecipe/>}/>
+        </Route>
+      </Routes>
+    </UserContextProvider>
+
+  )
+
 }
 
 export default App;
--- frontend/src/pages/IndexPage.js
+++ frontend/src/pages/IndexPage.js
@@ -1,9 +1,23 @@
-import Post from "../Post";
+import { useEffect } from "react"
+import Recipe from "../Recipe"
 
 export default function IndexPage() {
+
+    const [recipes, setRecipes] = useState([])
+    
+    useEffect(() => {
+        fetch('http://localhost:4000/post').then(response => {
+            response.json().then(recipes => {
+                setRecipes(recipes)
+            })
+        })
+    }, [])
+
     return (
         <>
-            <Post />
+            {recipes.length > 0 && recipes.map(recipe => (
+                <Recipe {...recipe} />
+            ))}
         </>
     )
 }
